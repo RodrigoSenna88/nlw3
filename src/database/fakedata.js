fef128e4a4122a54d84e5d4960472b1606efd53a
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-10-15 13:10:26 -0300

    atualizando branch. problenas no orphanages.hbs para acessar os dados do fakedata.js - (aula 4 em andamento)

---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports [
+  {
+    id: 1,
+    lat: "-22.9481192", 
+    lng: "-43.1888842",
+    name: "Lar das meninas",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de riesco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1597730945481-f35a5cf39021?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1600711724564-526eda91ac29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 8h até as 18h",
+    open_on_weekends: "1"
+  },
+  {
+    id: 2,
+    lat: "-22.9481192", 
+    lng: "-43.1988842",
+    name: "Lar dos meninos",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de riesco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1597730945481-f35a5cf39021?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1600711724564-526eda91ac29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 8h até as 18h",
+    open_on_weekends: "0"
+  }
+]
--- src/pages.js
+++ src/pages.js
@@ -1,18 +1,20 @@
+const orphanages = require('./database/fakedata.js');
+
 module.exports = {
 
   index(req, res) {
-    return res.render("index");
+    return res.render('index');
   },
 
   orphanage(req, res) {
-    return res.render("orphanage");
+    return res.render('orphanage');
   },
 
   orphanages(req, res) {
-    return res.render("orphanages");
+    return res.render('orphanages', { orphanages });
   },
 
   createOrphanage(req, res) {
-    return res.render("create-orphanage");
+    return res.render('create-orphanage');
   }
 };
